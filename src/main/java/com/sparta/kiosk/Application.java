f1437e21a096bea12da38cc9206f92e57d71d4de
SeuneKeun <eiziong1@gmail.com>
2024-11-22 20:30:22 +0900

    refactor: MenuItem 리스트를 Menu 클래스가 관리하도록 변경
    - MenuFactory로 생성해준 Menu를 kiosk 클래스에 리스트로 넘겨준다
    - MenuItem 레코드 클래스로 변경

--- src/main/java/com/sparta/kiosk/Application.java
+++ src/main/java/com/sparta/kiosk/Application.java
@@ -1,8 +1,18 @@
 package com.sparta.kiosk;
 
+import com.sparta.kiosk.domain.Menu;
+import com.sparta.kiosk.factory.MenuFactory;
+import com.sparta.kiosk.service.Kiosk;
+
+import java.util.List;
+
 public class Application {
     public static void main(String[] args) {
-        Kiosk kiosk = new Kiosk();
+        Menu burgerMenu = MenuFactory.createBurgerMenu();
+        Menu drinkMenu = MenuFactory.createDrinkMenu();
+        Menu dessertMenu = MenuFactory.createDessertMenu();
+        Kiosk kiosk = new Kiosk(List.of(burgerMenu, drinkMenu, dessertMenu));
+
         kiosk.start();
     }
 }
--- src/main/java/com/sparta/kiosk/domain/MenuItem.java
+++ src/main/java/com/sparta/kiosk/domain/MenuItem.java
@@ -1,25 +1,4 @@
 package com.sparta.kiosk.domain;
 
-public class MenuItem {
-    private String name;
-    private Double price;
-    private String description;
-
-    public MenuItem(String name, Double price, String description) {
-        this.name = name;
-        this.price = price;
-        this.description = description;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public Double getPrice() {
-        return price;
-    }
-
-    public String getDescription() {
-        return description;
-    }
+public record MenuItem(String name, Double price, String description) {
 }
